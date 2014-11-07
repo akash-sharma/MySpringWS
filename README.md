6b8e07476a5dc3b56741024b1508c7fed116eeae
akash-sharma <akash777.sharma@gmail.com>
2014-11-07 19:39:20 +0530

    validating interceptor with spring ws

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 MySpringWS
 ==========
+spring web service => http://briansjavablog.blogspot.in/2013/01/spring-web-services-tutorial.html
+
+rules of xsd to wsdl => http://stackoverflow.com/questions/1155809/spring-ws-generates-wsdl-without-operations
+
+http://stackoverflow.com/questions/14382565/springs-payloadvalidatinginterceptor-evaluates-xml-uncorrect-invalid
+
+http://forum.spring.io/forum/spring-projects/integration/101572-intercept-soap-message-spring-integration-jaxb
+
--- src/main/java/com/akash/ws/endpoint/EmployeeSalaryDetailEndpoint.java
+++ src/main/java/com/akash/ws/endpoint/EmployeeSalaryDetailEndpoint.java
@@ -11,10 +11,13 @@ import com.akash.ws.data.emp.employee.EmployeeSalResponse;
 @Endpoint
 public class EmployeeSalaryDetailEndpoint {
 
+//	http://stackoverflow.com/questions/14382565/springs-payloadvalidatinginterceptor-evaluates-xml-uncorrect-invalid
+	
 	public static final String NAMESPACE_URI="http://emp.data.ws.akash.com/employee";
 	
 	@PayloadRoot(localPart="EmployeeSalRequest" , namespace=NAMESPACE_URI)
-	public @ResponsePayload EmployeeSalResponse anyRandomName(@RequestPayload EmployeeSalRequest esReq) {
+	public @ResponsePayload EmployeeSalResponse anyRandomName(@RequestPayload EmployeeSalRequest esReq) throws Exception {
+		
 		EmployeeSalResponse esRes=new EmployeeSalResponse();
 		esRes.setEmployeeSalary("200");
 		return esRes;
